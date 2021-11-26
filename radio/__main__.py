47a1ecef3551993d82c9e648c4fe13bf1006e5d8
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-11-26 07:33:30 +0530

    Add files via upload

--- radio/__main__.py
+++ radio/__main__.py
@@ -1,13 +1,13 @@
 from pyrogram import Client, idle
 from config import API_ID, API_HASH, BOT_TOKEN
-from radio.radio import app
+from bot.videoplayer import app
 
 bot = Client(
     ":memory:",
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="radio"),
+    plugins=dict(root="bot"),
 )
 
 bot.start()
