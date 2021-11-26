09afe063ff4d1c75c375d21d53ca108973c68b5e
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-11-26 07:50:59 +0530

    Update __main__.py

--- radio/__main__.py
+++ radio/__main__.py
@@ -1,13 +1,13 @@
 from pyrogram import Client, idle
 from config import API_ID, API_HASH, BOT_TOKEN
-from radio.videoplayer import app
+from radio.radio import app
 
 bot = Client(
     ":memory:",
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="bot"),
+    plugins=dict(root="radio"),
 )
 
 bot.start()
