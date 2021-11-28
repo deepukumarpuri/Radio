c924c1d5b6e295504e7b75077acd12628a71a058
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-11-28 15:26:17 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,17 +1,33 @@
 import os
+from os import getenv
+from dotenv import load_dotenv
+from helpers.uptools import fetch_heroku_git_url
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-SESSION_NAME = os.getenv("SESSION_NAME")
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+load_dotenv()
+que = {}
+admins = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME", "Veez Music")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/8628c642a266a22effd8c.png")
+THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/4c39fbb88932761913fff.png")
+AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/73e10ed6e2bd32b478de6.png")
+QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/b95c13eef1ebd14dbb458.png")
+CMD_IMG = getenv("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
+ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/c83b000f004f01897fe18.png")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
 BOT_USERNAME = getenv("BOT_USERNAME", "veezmusicbot")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "DKBOTZ")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "DKBOTZ")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "DKBOTZ")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "veezassistant1")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "VeezSupportGroup")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "levinachannel")
 # isi dengan username kamu tanpa simbol @
-OWNER_NAME = getenv("OWNER_NAME", "DKBOTZ")
+OWNER_NAME = getenv("OWNER_NAME", "dlwrml")
 # fill with your nickname
-ALIVE_NAME = getenv("ALIVE_NAME", "DKBOTZ")
+ALIVE_NAME = getenv("ALIVE_NAME", "Levina")
 # fill with your id as the owner of the bot
 OWNER_ID = int(os.environ.get("OWNER_ID"))
 DATABASE_URL = os.environ.get("DATABASE_URL")  # fill with your mongodb url
@@ -30,4 +46,3 @@ UPSTREAM_REPO = os.environ.get(
     "UPSTREAM_REPO", "https://github.com/levina-lab/VeezMusic"
 )
 HEROKU_URL = fetch_heroku_git_url(HEROKU_API_KEY, HEROKU_APP_NAME)
-
