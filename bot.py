5dc6dac3e778ff81fa0445ff55d553c10e09c432
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-12-06 07:44:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ class Bot(Client):
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             workers=50,
-            plugins={"root": "plugins"},
+            plugins={"root": "radio.owner"},
             sleep_threshold=5,
         )
 
