02e37518c3359e61be65e146b7f7c2b0608e89dc
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-12-06 16:01:41 +0530

    Update database.py

--- radio/database.py
+++ radio/database.py
@@ -74,6 +74,49 @@ class Database:
         return self.col.find({"ban_status.is_banned": True})
 
 
+    async def add_chat(self, chat, title):
+        chat = self.new_group(chat, title)
+        await self.grp.insert_one(chat)
+    
+
+    async def get_chat(self, chat):
+        chat = await self.grp.find_one({'id':int(chat)})
+        if not chat:
+            return False
+        else:
+            return chat.get('chat_status')
+    
+
+    async def re_enable_chat(self, id):
+        chat_status=dict(
+            is_disabled=False,
+            reason="",
+            )
+        await self.grp.update_one({'id': int(id)}, {'$set': {'chat_status': chat_status}})
+    
+
+    async def disable_chat(self, chat, reason="No Reason"):
+        chat_status=dict(
+            is_disabled=True,
+            reason=reason,
+            )
+        await self.grp.update_one({'id': int(chat)}, {'$set': {'chat_status': chat_status}})
+    
+
+    async def total_chat_count(self):
+        count = await self.grp.count_documents({})
+        return count
+    
+
+    async def get_all_chats(self):
+        return self.grp.find({})
+
+
+    async def get_db_size(self):
+        return (await self.db.command("dbstats"))['dataSize']
+
+
+
 # Database
 db = Database(DATABASE_URL, BOT_USERNAME)
 mongo_db_lmao = MongoClient(DATABASE_URL)
