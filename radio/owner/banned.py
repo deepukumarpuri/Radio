096155fe3ae75350e2a280678a8c3644547c3602
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-12-07 08:10:55 +0530

    Update banned.py

--- radio/owner/banned.py
+++ radio/owner/banned.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from utils import temp
 from pyrogram.types import Message
-from database.users_chats_db import db
+from radio.database import db
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from config import SUPPORT_CHAT
 async def banned_users(_, client, message: Message):
