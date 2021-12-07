2341a8344ac3332e5ca52844c3d82776f6923cdd
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-12-07 07:10:00 +0530

    Update database.py

--- radio/database.py
+++ radio/database.py
@@ -3,75 +3,90 @@ import datetime
 import motor.motor_asyncio
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
-from config import BOT_USERNAME, DATABASE_URL
+from config import DATABASE_NAME, DATABASE_URI
 
 
 class Database:
+    
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.users
+        self.grp = self.db.groups
 
-    def new_user(self, id):
+
+    def new_user(self, id, name):
         return dict(
-            id=id,
-            join_date=datetime.date.today().isoformat(),
+            id = id,
+            name = name,
             ban_status=dict(
                 is_banned=False,
-                ban_duration=0,
-                banned_on=datetime.date.max.isoformat(),
                 ban_reason="",
             ),
         )
 
-    async def add_user(self, id):
-        user = self.new_user(id)
+
+    def new_group(self, id, title):
+        return dict(
+            id = id,
+            title = title,
+            chat_status=dict(
+                is_disabled=False,
+                reason="",
+            ),
+        )
+    
+    async def add_user(self, id, name):
+        user = self.new_user(id, name)
         await self.col.insert_one(user)
-
+    
     async def is_user_exist(self, id):
-        user = await self.col.find_one({"id": int(id)})
+        user = await self.col.find_one({'id':int(id)})
         return bool(user)
-
+    
     async def total_users_count(self):
         count = await self.col.count_documents({})
         return count
-
-    async def get_all_users(self):
-        return self.col.find({})
-
-    async def delete_user(self, user_id):
-        await self.col.delete_many({"id": int(user_id)})
-
+    
     async def remove_ban(self, id):
         ban_status = dict(
             is_banned=False,
-            ban_duration=0,
-            banned_on=datetime.date.max.isoformat(),
-            ban_reason="",
+            ban_reason=''
         )
-        await self.col.update_one({"id": id}, {"$set": {"ban_status": ban_status}})
-
-    async def ban_user(self, user_id, ban_duration, ban_reason):
+        await self.col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
+    
+    async def ban_user(self, user_id, ban_reason="No Reason"):
         ban_status = dict(
             is_banned=True,
-            ban_duration=ban_duration,
-            banned_on=datetime.date.today().isoformat(),
-            ban_reason=ban_reason,
+            ban_reason=ban_reason
         )
-        await self.col.update_one({"id": user_id}, {"$set": {"ban_status": ban_status}})
+        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
 
     async def get_ban_status(self, id):
         default = dict(
             is_banned=False,
-            ban_duration=0,
-            banned_on=datetime.date.max.isoformat(),
-            ban_reason="",
+            ban_reason=''
         )
-        user = await self.col.find_one({"id": int(id)})
-        return user.get("ban_status", default)
+        user = await self.col.find_one({'id':int(id)})
+        if not user:
+            return default
+        return user.get('ban_status', default)
 
-    async def get_all_banned_users(self):
-        return self.col.find({"ban_status.is_banned": True})
+    async def get_all_users(self):
+        return self.col.find({})
+    
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+
+    async def get_banned(self):
+        users = self.col.find({'ban_status.is_banned': True})
+        chats = self.grp.find({'chat_status.is_disabled': True})
+        b_chats = [chat['id'] async for chat in chats]
+        b_users = [user['id'] async for user in users]
+        return b_users, b_chats
+    
 
 
     async def add_chat(self, chat, title):
