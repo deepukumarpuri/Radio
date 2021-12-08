03700b68379d2862c269c3c4c773efbbea046ea1
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-12-08 08:23:45 +0530

    Add files via upload

--- radio/start.py
+++ radio/start.py
@@ -1,24 +1,117 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, Chat, CallbackQuery
+from datetime import datetime
+from sys import version_info
+from time import time
 
-@Client.on_message(filters.command("use"))
-async def start(client, m: Message):
-   if m.chat.type == 'private':
-      await m.reply(f"✨ **Hello, I Am a Telegram Most Powerful RADIO Bot.**\n\n💭 **I Was Created To Play Radio in Group Voice chats easily.**\n\n❔ **To Find Out How To Use Me, Press The Help Button Below** 👇🏻\n Must Be Read Terms & Condition For Bot Adding in Your Group",
-                    reply_markup=InlineKeyboardMarkup(
-                       [[
-                          InlineKeyboardButton(
-                             "❔ HOW TO USE THIS BOT", callback_data="cbguide")
-                       ],[
-                          InlineKeyboardButton(
-                             "🌐 Terms & Condition", callback_data="cbinfo")
-                       ],[
-                          InlineKeyboardButton(
-                             "👩🏻‍💻 Developer", url="https://t.me/DKBOTZHELP")
-                       ],[
-                          InlineKeyboardButton(
-                             "💭 Group", url="https://t.me/DK_BOTZ"),
-                          InlineKeyboardButton(
-                             "✨ Channel", url="https://t.me/DKBOTZ")
-                       ]]
-                    ))
+from config import (
+    ALIVE_IMG,
+    ALIVE_NAME,
+    BOT_NAME,
+    BOT_USERNAME,
+    GROUP_SUPPORT,
+    OWNER_NAME,
+    ASSISTANT_USERNAME,
+    ASSISTANT_NAME,
+    UPDATES_CHANNEL,
+    RADIO_BOT,
+)
+from radio import __version__
+from radio.decorators import sudo_users_only
+from radio.filters import command
+from pyrogram import Client, filters
+from pyrogram import __version__ as pyrover
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+__major__ = 0
+__minor__ = 2
+__micro__ = 1
+
+__python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ("week", 60 * 60 * 24 * 7),
+    ("day", 60 * 60 * 24),
+    ("hour", 60 * 60),
+    ("min", 60),
+    ("sec", 1),
+)
+
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return "inf"
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else "s"))
+    return ", ".join(parts)
+
+
+
+
+@Client.on_message(
+    command(["start", f"start@{BOT_USERNAME}"]) & filters.group & ~filters.edited
+)
+async def start_group(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+
+    keyboard = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("✨ Group", url=f"https://t.me/{GROUP_SUPPORT}"),
+                InlineKeyboardButton(
+                    "📣 Channel", url=f"https://t.me/{UPDATES_CHANNEL}"
+                ),
+            ]
+        ]
+    )
+
+    alive = f"**Hello {message.from_user.mention()}, i'm {BOT_NAME}**\n\n✨ Bot is Working Normally\n\n Bot Assistant: [{ASSISTANT_NAME}](https://t.me/{ASSISTANT_USERNAME})\n\n🍀 My Master: [{ALIVE_NAME}](https://t.me/{OWNER_NAME})\n✨ Bot Version: `v{__version__}`\n🍀 Pyrogram Version: `{pyrover}`\n✨ Python Version: `{__python_version__}`\n🍀 Uptime Status: `{uptime}`\n\n**Thanks for Adding me here, For Playing Radio in VC\n\nNote :- Without My Bot Assisant i Cannot Play Radio in Vc\n Contact To My Master And Buy Bot Premium. My Master is [{ALIVE_NAME}](https://t.me/{OWNER_NAME})** ❤"
+
+    await message.reply_photo(
+        photo=f"{ALIVE_IMG}",
+        caption=alive,
+        reply_markup=keyboard,
+    )
+
+
+@Client.on_message(
+    command(["help", f"help@{BOT_USERNAME}"]) & filters.group & ~filters.edited
+)
+async def help(client: Client, message: Message):
+    await message.reply_text(
+        f"""✨ **Hello** {message.from_user.mention()} !
+
+» **Press The Below Button To See The List Of Available Commands !**
+
+⚡ __Powered by {BOT_NAME} A.I__""",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="📚 Commands", callback_data="dkcmd")]]
+        ),
+    )
+
+
+@Client.on_message(command(["ping", f"ping@{BOT_USERNAME}"]) & ~filters.edited)
+async def ping_pong(client: Client, message: Message):
+    start = time()
+    m_reply = await message.reply_text("pinging...")
+    delta_ping = time() - start
+    await m_reply.edit_text("🏓 `PONG!!`\n" f"⚡️ `{delta_ping * 1000:.3f} ms`")
+
+
+@Client.on_message(command(["uptime", f"uptime@{BOT_USERNAME}"]) & ~filters.edited)
+@sudo_users_only
+async def get_uptime(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await message.reply_text(
+        "🤖 Bot Status:\n"
+        f"• **Uptime:** `{uptime}`\n"
+        f"• **Start Time:** `{START_TIME_ISO}`"
+    )
