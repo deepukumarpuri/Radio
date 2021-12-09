0ea3536f5ee4139fba7270151ab5de98e178af8a
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-12-09 18:11:19 +0530

    Add files via upload

--- radio/owner/commands.py
+++ radio/owner/commands.py
@@ -77,6 +77,29 @@ async def start(client, message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, Script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
+    if len(message.command) != 2:
+        buttons = [[
+            InlineKeyboardButton('❔ HOW TO USE THIS BOT', callback_data='cbguide')
+            ],[
+            InlineKeyboardButton('📚 Commands', callback_data='dkcmd'),
+            InlineKeyboardButton('🌐 Terms & Condition', callback_data='about')
+            ],[
+            InlineKeyboardButton('❤️ Donate', url=f'https://t.me/{OWNER_NAME}')
+            ],[
+            InlineKeyboardButton('📣 Official Channel', url='https://t.me/DKBOTZNETWORK'),
+            InlineKeyboardButton('✨ NEW Features', callback_data='cbfeature')
+            ],[
+            InlineKeyboardButton('✗ Close the Menu ✗', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_photo(
+            photo=random.choice(PICS),
+            caption=Script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            reply_markup=reply_markup,
+            quote=True,
+            parse_mode='html'
+        )
+        return
     if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
             invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
