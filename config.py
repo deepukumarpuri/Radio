5d8f49dd6b0ad144de48dd130ab2e45fc3aa82e9
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-12-16 07:16:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ PICS = list(getenv("PICS", "https://telegra.ph/file/8b42f6caf6ef5fd76766f.jpg ht
 
 #OWNERS CONFIG
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-PRO_USERS = list(map(int, getenv("PRO_USERS").split()))
+PRO_USERS = int(os.environ.get("PRO_USERS"))
 NOOB_USERS = list(map(int, getenv("NOOB_USERS").split()))
 OWNER_ID = int(os.environ.get("OWNER_ID"))
 ADMINS = int(os.environ.get("OWNER_ID"))
