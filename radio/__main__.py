5319eeb8eb190fbe3e0c214de86c596fd298e6ec
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-12-16 07:53:57 +0530

    Add files via upload

--- radio/__main__.py
+++ radio/__main__.py
@@ -1,16 +1,6 @@
 from pyrogram import Client, idle
-# the logging things
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-# the secret configuration specific things
-if bool(os.environ.get("WEBHOOK", False)):
-from config import API_ID, API_HASH, BOT_TOKEN, PRO_USERS
+from config import API_ID, API_HASH, BOT_TOKEN
 from radio.radio import app
-import pyrogram
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
 
 bot = Client(
     ":memory:",
@@ -19,8 +9,7 @@ bot = Client(
     bot_token=BOT_TOKEN,
     plugins=dict(root="radio"),
 )
-    PRO_USERS.add(184752635)
-    
+
 bot.start()
 app.start()
 idle()
